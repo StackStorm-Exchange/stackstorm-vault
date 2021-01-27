f4e05cfd0750bdafd2cad22ac5803bf1035fa842
Jacob Floyd <cognifloyd@gmail.com>
2021-01-27 17:21:09 -0600

    add tests for is_initialized action

--- tests/test_action_is_initialized.py
+++ tests/test_action_is_initialized.py
@@ -5,6 +5,20 @@ from tests.vault_action_tests_base import VaultActionTestCase
 class IsInitializedActionTestCase(VaultActionTestCase):
     action_cls = VaultIsInitializedAction
 
-    def test_method(self):
+    def test_already_initialized(self):
         action = self.get_action_instance(config=self.dummy_pack_config)
         result = action.run()
+        assert result is True
+
+    def test_before_and_after_initialization(self):
+        self.manager.restart_vault_cluster(perform_init=False)
+
+        action = self.get_action_instance(config=self.dummy_pack_config)
+        result = action.run()
+        assert result is False
+
+        self.manager.restart_vault_cluster(perform_init=True)
+
+        action = self.get_action_instance(config=self.dummy_pack_config)
+        result = action.run()
+        assert result is True
