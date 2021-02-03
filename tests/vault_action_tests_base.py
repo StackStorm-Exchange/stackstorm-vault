06afabbb3fea358fa8592a141b76b83bfde59539
Jacob Floyd <cognifloyd@gmail.com>
2021-02-03 17:19:23 -0600

    super() call order does not matter, so prefer consistency

--- tests/vault_action_tests_base.py
+++ tests/vault_action_tests_base.py
@@ -47,9 +47,9 @@ class VaultActionTestCase(HvacIntegrationTestCase, BaseActionTestCase):
             )
 
     def tearDown(self):
+        super(VaultActionTestCase, self).setUp()
         # HvacIntegrationTestCase does not call super(), so we take care of that.
         super(HvacIntegrationTestCase, self).setUp()
-        super(VaultActionTestCase, self).setUp()
 
         self.dummy_pack_config = None
         if self.secret_v1:
